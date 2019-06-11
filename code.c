c96b105654edcaf15b413509525a2a0573bc115c
bhargav26reddy <51699459+bhargav26reddy@users.noreply.github.com>
2019-06-11 14:40:44 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int value;
+    scanf("%d",&value);
+    if(value==0)
+    printf("zero");
+    else if(value>0)
+        printf("Positive");
+    else
+        printf("Negative");
+        return 0;
+        }
